e551b697f1e650a7b46883b82811a0145d76eecb
Thomas Bittner <thomas.bittner@posteo.de>
2021-02-09 16:40:31 +0100

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,7 @@
+﻿namespace IconResourceDemo
+{
+	/// <summary>
+	///     Interaction logic for App.xaml
+	/// </summary>
+	public partial class App { }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace IconResourceDemo
+{
+	/// <summary>
+	///     Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow
+	{
+		public MainWindow()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
